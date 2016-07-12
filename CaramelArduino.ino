90229d2bcec4bcd9499dd7b6e7672c5be21d50a4
Camron Korell <Camron Korell>
2016-07-12 15:09:35 -0600

    Changed opening/closing time, prevented redundant actions

--- CaramelArduino.ino
+++ CaramelArduino.ino
@@ -2,14 +2,20 @@
 
 #include "SoftwareSerial.h"
 
-const int btPwrPin = 8;
 SoftwareSerial bt(2, 3); // TX/RX pins
 const int btKeyPin = 4;
 const int spkrPin = 5;
 const int doorForward = 6;
 const int doorReverse = 7;
+const int btPwrPin = 8;
+const int manualSwitchOpen = 9;
+const int manualSwitchClose = 10;
+
+bool opened;
  
 void setup()  {
+    opened = false;
+  
   	Serial.begin(38400);
   	bt.begin(9600);
   	
@@ -19,70 +25,88 @@ void setup()  {
   	pinMode(spkrPin, OUTPUT);
   	pinMode(doorForward, OUTPUT);
   	pinMode(doorReverse, OUTPUT);
+    pinMode(manualSwitchOpen, INPUT);
+    pinMode(manualSwitchClose, INPUT);
 }
 
 // Main commands
 void help() {
-	print("Commands:");
-	print("help - opens this menu");
-	print("open - opens door");
-	print("close - closes door");
-	print("alert - plays alert noise");
+	  print("Commands:");
+	  print("help - opens this menu");
+	  print("open - opens door");
+	  print("close - closes door");
+	  print("alert - plays alert noise");
 }
 
 void openDoor() {
-	print("Opening door...");
+    if (opened) {
+        print("You can't open an opened door you walnut.");
+        buzz(128);
+        buzz(128);
+        return;
+    }
+  
+	  print("Opening door...");
 	
-	buzz(165);
-	buzz(165);
-	buzz(165);
-	buzz(300);
+	  buzz(165);
+	  buzz(165);
+	  buzz(165);
+	  buzz(300);
 	
-	digitalWrite(doorForward, HIGH);
-	delay(7000);
-	digitalWrite(doorForward, LOW);
+	  digitalWrite(doorForward, HIGH);
+	  delay(5500);
+	  digitalWrite(doorForward, LOW);
 	
-	print("Door openend successfully!");
+	  print("Door openend successfully!");
+    opened = true;
 }
 
 void closeDoor() {
-	print("Closing door...");
+    if (!opened) {
+        print("You can't close a closed door you walnut.");
+        buzz(128);
+        buzz(128);
+        return;
+    }
+  
+	  print("Closing door...");
 	
-	buzz(165);
-	buzz(165);
-	buzz(165);
-	buzz(100);
+	  buzz(165);
+	  buzz(165);
+	  buzz(165);
+	  buzz(100);
 	
-	digitalWrite(doorReverse, HIGH);
-	delay(7000);
-	digitalWrite(doorReverse, LOW);
+	  digitalWrite(doorReverse, HIGH);
+	  delay(5500);
+	  digitalWrite(doorReverse, LOW);
 	
-	print("Door closed successfully!");
+	  print("Door closed successfully!");
+    opened = false;
 }
 
 void alert() {
-	print("Alerting...");
+	  print("Alerting...");
 	
-	buzz(222);
-	buzz(222);
-	buzz(222);
-	buzz(222);
+	  buzz(222);
+	  buzz(222);
+	  buzz(222);
+	  buzz(222);
 	
-	print("Successfully alerted.");
+	  print("Successfully alerted.");
 }
 
 // Helper methods
 
 void print(String s) {
-	Serial.println(s);
+	  Serial.println(s);
     bt.println(s);
 }
 
 void buzz(int pitch) {
-	tone(spkrPin, pitch, 250);
-	delay(250);
-	noTone(spkrPin);
-	delay(5);
+	  tone(spkrPin, pitch, 250);
+	  delay(250);
+	  noTone(spkrPin);
+	  delay(5);
 }
 
 String getMessage() {
@@ -98,25 +122,25 @@ String getMessage() {
 }
 
 void okDisconnect() {
-	print("$okDisconnect");
+	  print("$okDisconnect");
 }
 
 void command() {
-	Serial.println("AT enabled. Remember to restart Arduino.");
-	bt.end();
-	digitalWrite(btKeyPin, HIGH);
-	digitalWrite(btPwrPin, LOW);
-	delay(200);
-	digitalWrite(btPwrPin, HIGH);
-	bt.begin(38400);
+	  Serial.println("AT enabled. Remember to restart Arduino.");
+	  bt.end();
+	  digitalWrite(btKeyPin, HIGH);
+	  digitalWrite(btPwrPin, LOW);
+	  delay(200);
+	  digitalWrite(btPwrPin, HIGH);
+	  bt.begin(38400);
 	
-	while (true) {
-		if (bt.available())
-    		Serial.write(bt.read());
+	  while (true) {
+		    if (bt.available())
+    		    Serial.write(bt.read());
 
-  		if (Serial.available())
-    		bt.write(Serial.read());
-	}
+  		  if (Serial.available())
+    		    bt.write(Serial.read());
+	  }
 }
 
 // Meat
@@ -126,6 +150,16 @@ void loop() {
     
     if (Serial.available() && Serial.read() == '#')
     	command();
+
+    if (digitalRead(manualSwitchClose) == HIGH) {
+        msg = "close";
+        print("SWITCH CLOSING");
+    }
+
+    if (digitalRead(manualSwitchOpen) == LOW) {
+        msg = "open";   
+        print("SWITCH OPENING");
+    }
     
     // Command filters here via else if's
     if (msg.startsWith("help")) 
